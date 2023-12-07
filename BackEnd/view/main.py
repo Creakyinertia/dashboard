cf236f15d959d5eeb91505054d7b12c6b61f9ba5
Creakyinertia <darshanvgowda1813@gmail.com>
2023-12-07 12:10:48 +0530

    first_commit

---
+++ BackEnd/view/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+from routers import employees
+
+app = FastAPI()
+
+
+app.include_router(employees.router)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello Bigger Applications!"}
