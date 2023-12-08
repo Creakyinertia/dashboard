bb68d5a3eff0c554cf5e70c6f2f0fcc8a73f28ae
Creakyinertia <darshanvgowda1813@gmail.com>
2023-12-08 12:38:51 +0530

    backendpack2

--- BackEnd/view/main.py
+++ BackEnd/view/main.py
@@ -1,13 +1,14 @@
 from fastapi import  FastAPI
+from shared.database import Base , engine
+from .routers import employees,customers
 
-from .routers import employees
 
 app = FastAPI()
-
+Base.metadata.create_all(bind=engine)
 
 app.include_router(employees.router)
+app.include_router(customers.router)
 
 @app.get("/")
 async def root():
     return {"message": "Hello Bigger Applications!"}
-
